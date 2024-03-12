45ec6ad5bd243516e7944ff006a21d76b8ddbd13
Jacek Kaczmarek <infinite.tech.poznan@gmail.com>
2024-03-12 11:44:34 +0100

    feat: add configuration controller

---
+++ src/main/java/com/energysolution/iot/controller/ConfigurationController.java
@@ -0,0 +1,39 @@
+package com.energysolution.iot.controller;
+
+import com.energysolution.iot.dto.ConfigurationRequest;
+import com.energysolution.iot.dto.ConfigurationResponse;
+import com.energysolution.iot.service.ConfigurationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/configurations")
+public class ConfigurationController {
+
+    private final ConfigurationService configurationService;
+
+    @Autowired
+    public ConfigurationController(ConfigurationService configurationService) {
+        this.configurationService = configurationService;
+    }
+
+    @PostMapping
+    public ConfigurationResponse createConfiguration(@RequestBody ConfigurationRequest request) {
+        return configurationService.createConfiguration(request.deviceId(), request.configuration());
+    }
+
+    @GetMapping("/{id}")
+    public ConfigurationResponse readConfiguration(@PathVariable Long id) {
+        return configurationService.readConfiguration(id);
+    }
+
+    @PutMapping("/{id}")
+    public ConfigurationResponse updateConfiguration(@PathVariable Long id, @RequestBody ConfigurationRequest request) {
+        return configurationService.updateConfiguration(id, request.configuration());
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteConfiguration(@PathVariable Long id) {
+        configurationService.deleteConfiguration(id);
+    }
+}
